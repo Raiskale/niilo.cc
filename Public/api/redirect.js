cfbd05f6f7522cec919fbe7acbc22b12f9e0de73
Niilo Elias Räisänen <s4rani02@students.osao.fi>
2025-01-30 10:19:57 +0200

    bang

--- Public/api/redirect.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res) => {
-    const userLanguage = req.headers['accept-language'] || 'en';  // Default to 'en' if no language detected
-    const language = userLanguage.startsWith('fi') ? 'fi' : 'en';  // Detect Finnish or default to English
-    
-    // Redirect user to the appropriate language version
-    res.writeHead(302, { Location: `/${language}` });
-    res.end();
-  };
-  
